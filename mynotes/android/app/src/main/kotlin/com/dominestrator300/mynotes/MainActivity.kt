39eaa86f1b6b581af300b07c13812650f0520f36
Hamza Algobba <hamzaalgobba@aucegypt.edu>
2023-09-08 00:03:19 +0300

    added my flutter projects

---
+++ mynotes/android/app/src/main/kotlin/com/dominestrator300/mynotes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dominestrator300.mynotes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mynotes/lib/main.dart
@@ -0,0 +1,64 @@
+
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:mynotes/views/login_view.dart';
+import 'package:mynotes/views/register_view.dart';
+import 'firebase_options.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        
+        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 59, 16, 229)),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    )
+    );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+        backgroundColor: Color.fromARGB(255, 9, 70, 41),
+        foregroundColor: Color.fromARGB(255, 255, 255, 255),
+        ),
+        body: FutureBuilder(
+          future: Firebase.initializeApp(
+                  options: DefaultFirebaseOptions.currentPlatform,
+                ),
+          builder: (context, snapshot)
+          
+          {
+            
+            switch (snapshot.connectionState)
+            {
+              case ConnectionState.done:
+                final user = FirebaseAuth.instance.currentUser;
+                if(user?.emailVerified ?? false)
+                {
+                  print("Your email is verified");
+                }
+                else {
+                  print('Your email is not verified');
+                }
+                return const Text('Done');
+              default:
+                return const Text('Loading...');
+            }
+            
+            
+          }
+        ),
+    );
+  }
+}
